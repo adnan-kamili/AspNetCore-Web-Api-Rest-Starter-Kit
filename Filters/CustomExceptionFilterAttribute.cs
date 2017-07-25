62ea72ca4ece612f195c5b857a86b2d613978472
Adnan Kamili <adnan.kamili@pearson.com>
2017-07-25 17:48:36 +0530

    added logger to exception handler

--- Filters/CustomExceptionFilterAttribute.cs
+++ Filters/CustomExceptionFilterAttribute.cs
@@ -3,27 +3,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace SampleApi.Filters
 {
     public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
     {
         private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly ILogger _logger;
 
-        public CustomExceptionFilterAttribute(IHostingEnvironment hostingEnvironment)
+        public CustomExceptionFilterAttribute(IHostingEnvironment hostingEnvironment, ILogger<CustomExceptionFilterAttribute> logger)
         {
             _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
         }
 
         public override void OnException(ExceptionContext context)
         {
             if (!_hostingEnvironment.IsDevelopment())
             {
-                // TODO: Log and report the exception to the developer
-                Console.WriteLine(_hostingEnvironment.EnvironmentName, context.Exception);
-
+                string message = "Oops! Something is broken, we are looking into it";
+                _logger.LogError(0, context.Exception, message);
                 context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                context.Result = new JsonResult(new { message = "Oops! Something is broken, we are looking into it" });
+                context.Result = new JsonResult(new { message = message });
             }
         }
     }
